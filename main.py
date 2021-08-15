2b4b706019c2dbdf7bc0116456d767aea001897a
Munib2002 <munib200002@gmail.com>
2021-08-15 12:36:04 +0500

    fixes indents

--- main.py
+++ main.py
@@ -10,23 +10,23 @@ cache = open(cachePath, 'r')
 cacheData = json.load(cache)
 
 for c in cacheData:
-	if c['downloaded']:
-		continue
-	
-	links = open('./anime/' + c['name'] + '.json')
-	linksData = json.load(links)
+    if c['downloaded']:
+        continue
 
-	i = 1
+    links = open('./anime/' + c['name'] + '.json')
+    linksData = json.load(links)
 
-	for link in linksData['links']:
-		url = link['url']
-		referrer = link['referrer']
-		name = linksData['name'] + ' -- ep ' + str(i) + '.mp4'
+    i = 1
 
-		i += 1
-		downloader.download(url, 'E:\\Anime\\' +
-							linksData['name'], output=name, referrer=referrer, confirm=False)
-	c['downloaded'] = True
+    for link in linksData['links']:
+        url = link['url']
+        referrer = link['referrer']
+        name = linksData['name'] + ' -- ep ' + str(i) + '.mp4'
+
+        i += 1
+        downloader.download(url, 'E:\\Anime\\' +
+                            linksData['name'], output=name, referrer=referrer, confirm=False)
+    c['downloaded'] = True
 
 cache = open(cachePath, 'w')
 
